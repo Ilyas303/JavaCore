88a016556b3a93d9ef93933e627b6bdccff66a6b
Ilyas303 <itakaev@list.ru>
2025-03-30 12:05:35 +0300

    hm

---
+++ src/main/java/homeworks/homework18/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.model.Users;
+import org.example.service.UsersService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class Main {
+
+    private final UsersService usersService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            Users user = usersService.getUser(1L);
+            log.info("Пользователь: {}", user);
+        } catch (Exception e) {
+            log.error("Ошибка при получении пользователя: {}", e.getMessage());
+        }
+    }
+}
